3e292b425deb296d8b86042597822e48d2407f0e
Joey Chen <jay.hatelove@gmail.com>
2023-12-09 12:59:04 +0800

    clean up

--- OneBackComboTrainingWeb/Domains/MatchResult.cs
+++ OneBackComboTrainingWeb/Domains/MatchResult.cs
@@ -28,19 +28,6 @@ public class MatchResult
     public void CancelHomeGoal()
     {
         CancelGoal('H');
-        // var isNextPeriod = false;
-        // if (_matchResult.EndsWith(';'))
-        // {
-        //     isNextPeriod = true;
-        //     _matchResult = _matchResult[..^1];
-        // }
-        //
-        // if (!_matchResult.EndsWith('H'))
-        // {
-        //     throw new MatchResultException() { MatchResult = this };
-        // }
-        //
-        // _matchResult = _matchResult[..^1] + (isNextPeriod ? ";" : "");
     }
 
     public string GetDisplayScore()
