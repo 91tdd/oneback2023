197333cb83bbdac47849119cdf3374eafb788228
Joey Chen <jay.hatelove@gmail.com>
2023-12-09 12:36:42 +0800

    feat: HomeGoal() and homeScore of GetDisplayScore()

--- OneBackComboTrainingWeb/Domains/MatchResult.cs
+++ OneBackComboTrainingWeb/Domains/MatchResult.cs
@@ -2,22 +2,23 @@
 
 public class MatchResult
 {
-    private readonly string _matchResult;
+    private string _matchResult;
 
     public MatchResult(string matchResult)
     {
         _matchResult = matchResult;
     }
 
-    public void HomeGoal()
-    {
-        throw new NotImplementedException();
-    }
-
     public string GetDisplayScore()
     {
-        var homeScore = "1";
+        var homeScore = _matchResult.Count(c => c == 'H');
+        // var homeScore = "1";
         var awayScore = "0";
         return $"{homeScore}:{awayScore} (First Half)";
+    }
+
+    public void HomeGoal()
+    {
+        _matchResult += "H";
     }
 }
